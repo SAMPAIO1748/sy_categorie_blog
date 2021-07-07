59a48761692972d676942e13649ff5383e98d235
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-07 11:11:33 +0200

    Ajout ArticleRepository et ArticleController

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,33 @@
+<?php
+
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/articles", name="articlesList")
+     */
+    public function articlesList(ArticleRepository $articleRepository)
+    {
+        $articles = $articleRepository->findAll();
+        return $this->render('articles.html.twig', ['articles' => $articles]);
+    }
+
+    /**
+     * @Route("/article/{id}", name="articleShow")
+     */
+    public function articleShow($id, ArticleRepository $articleRepository)
+    {
+        $article = $articleRepository->find($id);
+        return $this->render('article.html.twig', ['article' => $article]);
+    }
+
+}
