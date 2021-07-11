8b27c95b77ee76f11d45549c9623d0327e38eeef
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-11 11:36:08 +0200

    CRUD Categorie et Tag

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -3,8 +3,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Category;
 use App\Repository\CategoryRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -41,5 +44,78 @@ class CategoryController extends AbstractController
         }else{
             throw new NotFoundHttpException("Erreur 404. La page que vous cherchez n'a pas été trouvée");
         }
+    }
+
+    /**
+     * @Route("/categories/add", name="categoryAdd")
+     */
+    public function categoryAdd(CategoryRepository $categoryRepository)
+    {
+        return $this->render('categoryadd.html.twig');
+    }
+
+    /**
+     * @Route("/categories/insert", name="categoryInsert")
+     */
+    public function categoryInsert(EntityManagerInterface $entityManager, Request $request)
+    {
+        $title = $request->request->get('title');
+        $description = $request->request->get('description');
+
+        $category = new Category();
+        $category->setTitle($title);
+        $category->setDescription($description);
+
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('categoriesList');
+
+    }
+
+    /**
+     * @Route ("/category/delete/{id}" , name="categoryDelete")
+     */
+    public function categoryDelete($id, categoryRepository $categoryRepository, EntityManagerInterface $entityManager)
+    {
+        $category = $categoryRepository->find($id);
+        $entityManager->remove($category);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('categoriesList');
+    }
+
+    /**
+     * @Route("/category/update/{id}", name="categoryUpdate")
+     */
+    public function categoryUpadte($id, categoryRepository $categoryRepository )
+    {
+        $category = $categoryRepository->find($id);
+        
+
+        return $this->render('categoryupdate.html.twig', ['category' => $category]);
+    }
+
+    /**
+     * @Route("/category/save/{id}", name="categorySave")
+     */
+    public function categorySave($id, Request $request, categoryRepository $categoryRepository, EntityManagerInterface $entityManager)
+    {
+        $category = $categoryRepository->find($id);
+
+        $title = $request->request->get('title');
+        $description = $request->request->get('description');
+
+        $category->setTitle($title);
+        $category->setDescription($description);
+       
+
+        $entityManager->persist($category);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('categoriesList');
+
+
     }
 }
--- src/Controller/TagController.php
+++ src/Controller/TagController.php
@@ -3,8 +3,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Tag;
 use App\Repository\TagRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpFoundation\Response;
@@ -16,7 +19,13 @@ class TagController extends AbstractController
         'red' => 'rouge',
         'blue' => 'bleu',
         'green' => 'vert',
-        'orange' => 'orange'
+        'orange' => 'orange',
+        'black' => 'noir',
+        'brown' => 'marron',
+        'yellow' => 'jaune',
+        'pink' => 'rose',
+        'violet' => 'violet',
+        'grey' => 'gris',
     ];
     /**
      * @Route ("/tags", name="tagList")
@@ -42,6 +51,79 @@ class TagController extends AbstractController
         }else{
             throw new NotFoundHttpException("Erreur 404. La page que vous cherchez n'a pas été trouvée");
         }
+    }
+
+    /**
+     * @Route("/tags/add", name="tagAdd")
+     */
+    public function tagAdd(tagRepository $tagRepository)
+    {
+        return $this->render('tagadd.html.twig');
+    }
+
+    /**
+     * @Route("/tags/insert", name="tagInsert")
+     */
+    public function tagInsert(EntityManagerInterface $entityManager, Request $request)
+    {
+        $title = $request->request->get('title');
+        $color = $request->request->get('color');
+
+        $tag = new Tag();
+        $tag->setTitle($title);
+        $tag->setColor($color);
+
+
+        $entityManager->persist($tag);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('tagList');
+
+    }
+
+    /**
+     * @Route ("/tag/delete/{id}" , name="tagDelete")
+     */
+    public function tagDelete($id, tagRepository $tagRepository, EntityManagerInterface $entityManager)
+    {
+        $tag = $tagRepository->find($id);
+        $entityManager->remove($tag);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('tagList');
+    }
+
+    /**
+     * @Route("/tag/update/{id}", name="tagUpdate")
+     */
+    public function tagUpadte($id, tagRepository $tagRepository )
+    {
+        $tag = $tagRepository->find($id);
+
+
+        return $this->render('tagupdate.html.twig', ['tag' => $tag]);
+    }
+
+    /**
+     * @Route("/tag/save/{id}", name="tagSave")
+     */
+    public function tagSave($id, Request $request, tagRepository $tagRepository, EntityManagerInterface $entityManager)
+    {
+        $tag = $tagRepository->find($id);
+
+        $title = $request->request->get('title');
+        $color = $request->request->get('color');
+
+        $tag->setTitle($title);
+        $tag->setColor($color);
+
+
+        $entityManager->persist($tag);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('tagList');
+
+
     }
 
 }
