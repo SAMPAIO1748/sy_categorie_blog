10d13fe6179df59d261bf5aae54a85ce9745010a
SAMPAIO1748 <57705851+SAMPAIO1748@users.noreply.github.com>
2021-07-15 12:24:49 +0200

    Category et tag disponibles pour la création d'un article

--- src/Form/ArticleType.php
+++ src/Form/ArticleType.php
@@ -3,8 +3,11 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Tag;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,8 +20,18 @@ class ArticleType extends AbstractType
             ->add('content')
             ->add('createdAt')
             ->add('isPublished')
-            //->add('category')
-            //->add('tag')
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title',
+                'multiple' => false,
+                'expanded' => false
+            ])
+            ->add('tag',EntityType::class, [
+            'class' => Tag::class,
+            'choice_label' => 'title',
+            'multiple' => false,
+            'expanded' => false
+            ])
             ->add('submit', SubmitType::class)
         ;
     }
